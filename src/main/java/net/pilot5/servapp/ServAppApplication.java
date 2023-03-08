fe2a34e64d5807b5658a6d6b6acd65505975d3f4
a184ot <a184ot@gmail.com>
2023-03-08 22:20:25 +0600

    Initial commit

---
+++ src/main/java/net/pilot5/servapp/ServAppApplication.java
@@ -0,0 +1,13 @@
+package net.pilot5.servapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/pilot5/servapp/ServAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.pilot5.servapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
